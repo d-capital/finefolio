9e1c2520d139b6c872490ee9daa72f1a1a432ec5
DCAPITAL <Dcapital10!>
2020-06-14 16:43:50 +0300

    capital input handling

--- fm_bot.py
+++ fm_bot.py
@@ -1,12 +1,12 @@
 from config import *
 import telebot
 import fine_folio_1
-import os
+import re
 import flask
 
 user_capital_1 = 0
 
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(TOKEN_TEST)
 
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
 keyboard1.row('50 000 рублей', '100 000 рублей', '150 000 рублей', '200 000 рублей', '300 000 рублей', '400 000 рублей',
@@ -18,32 +18,41 @@ keyboard2.row('голубые фишки', 'топ-30')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, я бот который поможет тебе вложить деньги в портфель ценных '
+    bot.send_message(message.chat.id, 'Привет, я бот, который поможет Вам вложить деньги в портфель ценных '
                                       'бумаг на московской бирже. '
-                                      'Для начала давай поймем сколько денегь ты хочешь вложить в портфель?',
+                                      'Для начала давай поймем сколько денег Вы хотите вложить в портфель?',
                      reply_markup=keyboard1)
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     global user_capital_1
-    if send_text_0(message) > 0:
-        user_capital_1 = send_text_0(message)
-        send_text_2(message)
-    send_text_1(message, user_capital_1)
+    if type(send_text_0(message)) == int:
+        if send_text_0(message) > 0:
+            user_capital_1 = send_text_0(message)
+            send_text_2(message)
+    else: send_text_1(message, user_capital_1)
 
 def send_text_0(message, user_capital_from_message=0):
-    res = "рублей" in message.text
+    res = bool(re.findall('\d+', message.text))
     if res == True:
-        user_capital_from_message = get_capital(message.text)
-        return user_capital_from_message
-    return user_capital_from_message
+        try:
+            user_capital_from_message = get_capital(message.text)
+        except IndexError:
+            bot.send_message(message.chat.id, 'Нужно вести цифры, деньги любят счет.')
+        if user_capital_from_message >= 30000:
+            return user_capital_from_message
+        else:
+            bot.send_message(message.chat.id, 'Боюсь, это слишком мало, у большинства брокеров минимальный порог входа 30 000 рублей.'
+                                                'Вы только подумайте одна акция комапнии Норникел (GMKN) стоит около 18 000 рублей. '
+                                              'Попробуйте выбрать из варинатов на клавиатуре ниже.', reply_markup= keyboard1)
 
 @bot.message_handler(content_types=['text'])
 def send_text_2(message):
     bot.send_message(message.chat.id, 'Отлично, с вашим капиталом мы определились, '
-                                      'теперь нужн решить из каких акций московской биржи мы соберем ваш портфель '
-                                      '- из голубых фишек или из TOP-30?', reply_markup=keyboard2)
+                                      'теперь нужно решить из каких акций московской биржи мы соберем Ваш портфель '
+                                      '- из голубых фишек или из TOP-30? '
+                                      'Кстати, сейчас Вы можете поменять решение о капитале введя цифру в ответном сообщении.', reply_markup=keyboard2)
 
 @bot.message_handler(content_types=['text'])
 def send_text_1(message, user_capital, moex_list='moex_list', blue='blue_chips_moex'):
@@ -61,9 +70,9 @@ def send_text_1(message, user_capital, moex_list='moex_list', blue='blue_chips_m
 
 def get_capital(user_capital):
     user_capital = user_capital.replace(" ", "")
-    user_capital = user_capital.replace("рублей", "")
-    user_capital = int(user_capital)
+    user_capital = re.findall('\d+', user_capital)
+    user_capital = int(user_capital[0])
     return user_capital
 
 
-#bot.polling()
+bot.polling()
